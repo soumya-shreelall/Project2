704775cbf4a5fc469e01e24e0b4ef407c273a3a4
soumya lall <soumya lall@LAPTOP-CU2M7T7J>
2022-03-28 10:11:07 +0530

    first commit do it

---
+++ src/test/java/com/crm/autodesk/sinirla/CreateOrganizationTest.java
@@ -0,0 +1,11 @@
+package com.crm.autodesk.sinirla;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest {
+	@Test
+	public void createorg()
+	{
+System.out.println("organization is created");
+}
+}
