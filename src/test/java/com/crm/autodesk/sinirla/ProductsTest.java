001c6a58e0642b868328cc86b2cd2e65e8e14068
soumya lall <soumya lall@LAPTOP-CU2M7T7J>
2022-03-28 10:38:10 +0530

    2nd commit

---
+++ src/test/java/com/crm/autodesk/sinirla/ProductsTest.java
@@ -0,0 +1,12 @@
+package com.crm.autodesk.sinirla;
+
+import org.testng.annotations.Test;
+
+public class ProductsTest {
+	@Test
+	public void product()
+	{
+System.out.println("product page created");
+
+}
+}
